9fb383047f4dbda4a3bc3cbfc2f73f9e341c2507
gugamaxara <gugamaxara25@gmail.com>
2019-09-09 18:42:41 +0400

    added mail functions to order and header also

---
+++ src/helper_functions/axios_request.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const sendGetRequest = (customer_name, customer_email, customer_text, customer_phone, customer_task, customer_address) => {
+axios.get("http://localhost:3000/sendmail/", {params: {
+    customer_name: customer_name,
+    customer_email: customer_email,
+    customer_text: customer_text,
+    customer_phone: customer_phone,
+    customer_task: customer_task,
+    customer_address: customer_address
+}})
+.then(response => {
+    console.log(response)
+})
+.catch(error => {
+    console.log(error)
+
+})
+}
